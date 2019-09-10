3634e2f93bce1c84e38f1a1e0e0271843e5533f5
Derek-Xiang <31242021+Derek-Xiang@users.noreply.github.com>
2019-09-10 18:51:25 +1000

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -9,3 +9,5 @@ echo "===For the method loadSpec===:"
 python3 -m unittest testers.SpecTest.testSpec
 echo "===Test the method readRow===:"
 python3 -m unittest testers.SpecTest.testReadRow
+echo "===Test the method testCsvGenerator===:"
+python3 -m unittest testers.SpecTest.testCsvGenerator
