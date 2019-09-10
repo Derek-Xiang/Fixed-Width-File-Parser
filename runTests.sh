fd2ab55fc34c0064dc5972cb85205ce8a91612ca
Derek-Xiang <31242021+Derek-Xiang@users.noreply.github.com>
2019-09-10 19:08:53 +1000

    .sh file fixed

--- runTests.sh
+++ runTests.sh
@@ -2,12 +2,20 @@
 
 cd test
 
-echo "========For both tests========:"
+echo "========For all tests========:"
 python3 testers.py
-
 echo "===For the method loadSpec===:"
 python3 -m unittest testers.SpecTest.testSpec
 echo "===Test the method readRow===:"
 python3 -m unittest testers.SpecTest.testReadRow
+
 echo "===Test the method testCsvGenerator===:"
+echo 1.. generate file "generateInputFile.txt" by spec file
+cd ..
+python3 generateInputFile.py
+echo 2.. convert it to csv file ..
+python3 main.py newTestInput.txt newTestOutput.csv spec.json
+echo 3.. csv file "newTestOutput.csv" is generated ..
+echo 4.. test the testCsvGenerator method ..
+cd test
 python3 -m unittest testers.SpecTest.testCsvGenerator
